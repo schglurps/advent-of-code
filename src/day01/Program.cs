9e5cc43437597c790078281f189dc26eb9df9a88
bidulotron <farid.laoufi@outlook.com>
2024-12-01 08:32:20 +0100

    Day 1, part 1

---
+++ src/day01/Program.cs
@@ -0,0 +1,34 @@
+﻿const int length = 1000;
+
+int[] left = new int[length];
+int[] right = new int[length];
+
+using(StreamReader sr = new("input"))
+{
+    for(int i = 0; i < length; i++)
+    {
+        string line = (await sr.ReadLineAsync())!;
+        var span = line.AsSpan();
+        left[i] = int.Parse(span.Slice(0, 5));
+        right[i] = int.Parse(span.Slice(8, 5));
+    }
+}
+
+Array.Sort(left);
+Array.Sort(right);
+
+int result = left.Zip(right).Aggregate(
+    0,
+    (acc, cur) =>
+    {
+        var (first, second) = cur;
+        if(second > first)
+        {
+            (first, second) = (second, first);
+        }
+
+        return acc + first - second;
+    }
+);
+
+Console.WriteLine(result);
